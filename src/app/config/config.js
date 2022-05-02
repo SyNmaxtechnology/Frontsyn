ebd28c053d41fe199e54224a110b8172c4129663
SyNmaxtechnology <synmaxtechnology@gmail.com>
2022-05-02 08:18:18 -0600

    cambios pantalla facturacion

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,3 +1,3 @@
 import process  from '../../../variables.js';
-//export const baseURL = () => process.env.API_HEROKU_PROD;
-export const baseURL = () => process.env.API_LOCAL
+export const baseURL = () => process.env.API_HEROKU_PROD;
+//export const baseURL = () => process.env.API_LOCAL
