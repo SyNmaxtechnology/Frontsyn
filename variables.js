58363243b44ee23dafed986a829045ab75028c3f
SyN Max Technology <63320165+SyNmaxtechnology@users.noreply.github.com>
2023-02-06 21:29:21 -0600

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
         API_LOCAL : 'http://localhost:5000',
-        API_HEROKU_PROD: 'https://apifacturaelectronica.herokuapp.com',
+        API_HEROKU_PROD: 'https://appsyn.herokuapp.com',
         API_HEROKU_STAG: 'https://apifacturaelectronicapruebas.herokuapp.com'
     }
 }
